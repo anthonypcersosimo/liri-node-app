ec2ff128b05def5503a648bc7fb1c9a77e85c891
Anthony <anthony.cersosimo@gmail.com>
2019-06-03 23:45:19 -0400

    added functionality to store one of four commands, loop through command line argument and retrieve your desired song or movie and print those to the console

--- liri-app.js
+++ liri-app.js
@@ -4,5 +4,21 @@ var spotify = require('node-spotify-api');
 var dotenv = require('dotenv').config();
 var moment = require("moment");
 var keys = require("./keys.js");
+// variable to access Spotify keys
+var spotify = new spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+// Store command
+var command = process.argv[2];
+
+// Store argv as array
+var argument = process.argv;
+
+// Take in command line argument and store the desired song, artist etc... as a variable 'request'
+var tempArray = [];
+for (i = 3; i < argument.length; i++) {
+    tempArray.push(process.argv[i]);
+}
+var request = tempArray.join(" ");
+
+console.log("===============");
+console.log("So far we have your command: " + command + "\nand your request: " + request);
