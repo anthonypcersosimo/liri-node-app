72769c493e602146c02a8c826bdf5d3483513aa0
Anthony <anthony.cersosimo@gmail.com>
2019-06-05 22:19:11 -0400

    added if statements to test if argv cmd line arguments are the functions specified... then grab data from APIs. Building out the response from OMDB API

--- liri-app.js
+++ liri-app.js
@@ -22,3 +22,43 @@ var request = tempArray.join(" ");
 
 console.log("===============");
 console.log("So far we have your command: " + command + "\nand your request: " + request);
+
+if(!command) {
+    command = "spotify-this-song";
+}
+
+else if (!request) {
+    command = "spotify-this-song";
+    request = "The Sign";
+}
+
+else if (command === "concert-this") {
+    console.log("Searching for Concert");
+}
+else if (command === "spotify-this-song") {
+    console.log("Searching for Song");
+}
+else if (command === "movie-this") {
+    var findShow = function(request) {
+        // The following URL can be used to search the TV Maze API for a given show
+        var queryUrl = "http://www.omdbapi.com/?t=" + request + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl)
+        .then(function(response) {
+            var jsonData = response.data;
+            console.log(jsonData);
+            var movieData = [
+            "Title: " + jsonData.Title,
+            "Year: " + jsonData.Year,
+            "Genre(s): " + jsonData.Year,
+            "Rating: " + jsonData.rating.average,
+            "Network: " + jsonData.network.name,
+            "Summary: " + jsonData.summary
+            ].join("\n\n");
+            console.log(movieData);
+        });
+    };
+    findShow();
+}
+else if (command === "do-what-it-says") {
+    console.log("As you wish");
+};
